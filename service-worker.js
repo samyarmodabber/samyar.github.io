9f2d623c5733faaa6c529d7fb80425439dc29358
Samyar <samyar.modabber@gmail.com>
2019-06-05 10:10:00 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.3684e5a10efd819a010b8504772e1487.js"
+  "/portfolio/precache-manifest.369329cc8f58447a2c92aefab70cddfb.js"
 );
 
 self.addEventListener('message', (event) => {
